ff7089d1c1f3590c99b93f7a4649aa73e8151cb7
nHackel <nHackel@users.noreply.github.com>
2024-03-01 18:29:43 +0100

    Move definitions into .h file as otherwise Arduino cant compile

--- Arduino/lib/communication.h
+++ Arduino/lib/communication.h
@@ -15,22 +15,120 @@ typedef struct
 } commandCallback_t;
 
 
-template<int N = 256>
 class SerialHandler {
     
     public:
-    SerialHandler(commandCallback_t cmds[], int numCmds);
+    SerialHandler(int size, commandCallback_t cmds[], int numCmds) : buffer_size(size), cmds(cmds), cmd_size(numCmds) {
+        buffer_position = 0;
+        buffer = new char [buffer_size];
+    };
+    ~SerialHandler() {
+        delete buffer;
+    };
     int read();
 
     private:
     bool updateBufferUntilDelim();
-    static const int buffer_size = N;
+    const int buffer_size;
     int buffer_position;
-    char buffer[N];
+    char *buffer;
     
     int cmd_size;
     commandCallback_t *cmds;
 
 };
+
+#include "Arduino.h"
+#include "communication.h"
+
+bool SerialHandler::updateBufferUntilDelim()
+{
+    char nextChar;
+    while (Serial.available() > 0)
+    {
+        nextChar = Serial.read();
+        if (nextChar == CMD_DELIM)
+        {
+            buffer[buffer_position] = '\0';
+            buffer_position = 0;
+            return true;
+        }
+        else if (nextChar != '\n')
+        {
+            buffer[buffer_position % (buffer_size - 1)] = nextChar; // Size - 1 to always leave room for \0
+            buffer_position++;
+        }
+    }
+    return false;
+}
+
+int SerialHandler::read() // Serial communication with PC in particular julia communication
+{
+    int s = -1;
+    int e = -1;
+    char beginDelim = BEGIN_DELIM;
+    char *beginCmd;
+    char endDelim = END_DELIM;
+    char *endCmd;
+    char *command;
+    char *paramStart;
+    bool delimFound = false;
+    bool unknown = false;
+
+    // determin substring
+    if (Serial.available() > 0)
+    {
+        delimFound = updateBufferUntilDelim();
+    }
+
+    if (!delimFound)
+    {
+        return 0;
+    }
+
+    s = -1;
+    if ((beginCmd = strchr(buffer, beginDelim)) != NULL)
+    {
+        s = beginCmd - buffer;
+    }
+
+    e = -1;
+    if ((endCmd = strchr(buffer, endDelim)) != NULL)
+    {
+        e = endCmd - buffer;
+    }
+
+    unknown = true;
+    // check if valid command
+    if (e != -1 && s != -1)
+    {
+        command = beginCmd + 1;
+        *endCmd = '\0';
+        // check for known commands
+        /// Getting rid of parameters at the end of String
+        int commandOnly = 0;
+        paramStart = strchr(command, '<');
+        if (paramStart != NULL)
+        {
+            commandOnly = command - paramStart;
+        }
+        else
+        {
+            commandOnly = endCmd - command - 1;
+        }
+        for (int i = 0; i < cmd_size; i++)
+        {
+            if (strncmp(cmds[i].id, command, commandOnly) == 0)
+            {
+                cmds[i].callback(command);
+                unknown = false;
+                buffer[0] = '\0'; // "Empty" input buffer
+                return 0;
+            }
+        }
+    }
+
+    return -1;
+}
 
 #endif
--- Arduino/lib/rp_watchdog.h
+++ Arduino/lib/rp_watchdog.h
@@ -23,4 +23,47 @@ class RedPitayaWatchdog
     bool fail;
 };
 
+#include "Arduino.h"
+#include "rp_watchdog.h"
+
+void RedPitayaWatchdog::setup()
+{
+    pinMode(output, OUTPUT);
+    pinMode(input, INPUT);
+}
+
+void RedPitayaWatchdog::reset()
+{
+    value = true;
+    fail = false;
+    send();
+}
+
+
+bool RedPitayaWatchdog::hasFailed()
+{
+    return fail;
+}
+
+void RedPitayaWatchdog::send()
+{
+    digitalWrite(output, value);
+    time = micros();
+}
+
+bool RedPitayaWatchdog::check()
+{
+    if (digitalRead(input) == value)
+    {
+        fail = false;
+        value = !value;
+        send();
+    }
+    else if ((micros() - time) > timeout)
+    {
+        fail = true;
+    }
+    return fail;
+}
+
 #endif
